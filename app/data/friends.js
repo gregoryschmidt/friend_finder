da85e55352cab9d7f1e998ef257248dadc09f935
GREGORY SCHMIDT <gregoryschmidt@GREGORYs-MBP.lan>
2019-03-10 22:35:44 -0700

    server/rounting done

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var surveyArray = [
+{
+    "name":"Ahmed",
+    "photo":"https://github.com/gregoryschmidt/responsive-portfolio/blob/master/assets/images/goodmug1.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  }
+];
+
+module.exports = surveyArray;
---
+++ server.js
@@ -0,0 +1,28 @@
+// Dependencies
+// =============================================================
+var express = require('express')
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+/* respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  res.send('hello world')
+}) */
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
+
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
